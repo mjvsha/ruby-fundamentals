41ef97f9072897a10f1894aedaef190ebe2c931c
Meera <mjvsharma@outlook.com>
2017-09-18 15:07:18 -0400

    wrote programming comparing names

---
+++ exercise4c.rb
@@ -0,0 +1,9 @@
+puts "Yo what is your name??"
+user_name = gets.chomp
+my_name = "Meera"
+
+if user_name == my_name
+  puts "You have such a beautiful amazing and cute name"
+elsif
+  puts "Yikes"
+end
