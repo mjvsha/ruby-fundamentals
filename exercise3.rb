a545fcc96760d0d2c6ad804efd5e485b37fa795f
Meera <mjvsharma@outlook.com>
2017-09-18 14:29:07 -0400

    wrote program prompting for name and age

--- exercise3.rb
+++ exercise3.rb
@@ -2,4 +2,8 @@
 
 puts "What is your name homeboy?"
 user_name = gets.chomp #this removes the \n from the user input
-puts "Hello, #{user_name}!!!!!!!!!!!!"
+puts "Hey, #{user_name}!!!!!!!!!!!!"
+
+puts "How old are you old sport?"
+user_age= gets.chomp.to_i
+puts "Damn son that means you were born  in #{2017-user_age-1} or #{2017-user_age}!"
