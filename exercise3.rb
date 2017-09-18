ffc4d5082de2898648f4fd0e7f23bf02003ff6d8
Meera <mjvsharma@outlook.com>
2017-09-18 14:52:24 -0400

    exercise 4a

--- exercise3.rb
+++ exercise3.rb
@@ -7,3 +7,4 @@ puts "Hey, #{user_name}!!!!!!!!!!!!"
 puts "How old are you old sport?"
 user_age= gets.chomp.to_i
 puts "Damn son that means you were born  in #{2017-user_age-1} or #{2017-user_age}!"
+  
---
+++ exercise4a.rb
@@ -0,0 +1,11 @@
+
+puts "Provide me a number old sport"
+user_number = gets.chomp.to_i
+
+if user_number >= 100
+  puts "That's a big number old sport"
+
+elsif
+  puts "why dream a little bit bigger?"
+
+end 
